dcabf58159b4ce4beb4ef98fb7ea87b03a7a34a4
Kolton <Gamerunner15@gmail.com>
2025-06-02 14:03:37 -0400

    added logo.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  esbuild: {
+    loader: 'jsx',
+  },
   mode: "production",
    base: "/monsterBattlerReact/",
    build: {
