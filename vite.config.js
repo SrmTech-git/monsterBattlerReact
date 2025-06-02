7d2bcefda1063b8397ccac2a5971b309911904a7
Kolton <Gamerunner15@gmail.com>
2025-06-02 13:42:32 -0400

    trying to add main.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  mode: "production",
    base: "/monsterBattlerReact/",
    build: {
-    outDir: "build"
+    outDir: "build",
+    assetsDir: "static"
   }
 })
