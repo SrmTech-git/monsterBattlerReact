3757320d60f77b67f653ca156cb30a97881846b3
Kolton <Gamerunner15@gmail.com>
2025-06-02 14:10:33 -0400

    added main.js manually.

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Render the main App component inside the root div
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
